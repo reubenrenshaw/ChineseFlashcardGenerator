b6c91364ef2d5642d0d332933dada9515b5f65a1
Reuben Renshaw <rgr7466@g.rit.edu>
2024-07-30 16:03:23 -0500

    Fixed file errors

--- ChineseNovelFlashcardGenerator.py
+++ ChineseNovelFlashcardGenerator.py
@@ -15,7 +15,7 @@ converter = opencc.OpenCC('t2s.json')
 def main():
     char_dict = {}
     word_count = 0
-    with open('test.txt', 'r', encoding='utf-8', errors='replace') as file:
+    with open('text.txt', 'r', encoding='utf-8', errors='replace') as file:
         #test = '你是我的小苹果'
         unique_text = set()
         words = list(jieba.cut(converter.convert(file.read())))
@@ -29,14 +29,14 @@ def main():
         threads = []
         thread_count = 0
         print(str(len(unique_text) / block_size) + " threads created")
-        for i in range(0, num_blocks):
-            block = unique_text[i * block_size: i * block_size + block_size]
-            thread = threading.Thread(target = translate_block, args = (block, word_count))
-            threads.append(thread)
-            thread.start()
+    for i in range(0, num_blocks):
+        block = unique_text[i * block_size: i * block_size + block_size]
+        thread = threading.Thread(target = translate_block, args = (block, word_count))
+        threads.append(thread)
+        thread.start()
 
-        for thread in threads:
-            thread.join()
+    for thread in threads:
+        thread.join()
              
                 
     AnkiGenerator.package_deck()
