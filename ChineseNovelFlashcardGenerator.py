e38b19774f1a0a9cfce8c5d92bc9106c25db3aa5
Reuben Renshaw <rgr7466@g.rit.edu>
2024-07-30 15:29:28 -0500

    Changed generator, breaks when large files

--- ChineseNovelFlashcardGenerator.py
+++ ChineseNovelFlashcardGenerator.py
@@ -7,7 +7,7 @@ import jieba
 import threading
 
 # Block Size for threading
-block_size = 5000
+block_size = 100
 
 # opencc converter initializer
 converter = opencc.OpenCC('t2s.json')
@@ -15,8 +15,11 @@ converter = opencc.OpenCC('t2s.json')
 def main():
     char_dict = {}
     word_count = 0
-    with open('text.txt', 'r', encoding='utf-8', errors='replace') as file:
+    with open('test.txt', 'r', encoding='utf-8', errors='replace') as file:
+        #test = '你是我的小苹果'
+        unique_text = set()
         words = list(jieba.cut(converter.convert(file.read())))
+        #words = list(jieba.cut(test))
         unique_text = set()
         for word in words:
             unique_text.add(word)
@@ -24,6 +27,8 @@ def main():
         #unique_text = unique_text[0: 100]
         num_blocks = int((len(unique_text) / block_size) + 0.5)
         threads = []
+        thread_count = 0
+        print(str(len(unique_text) / block_size) + " threads created")
         for i in range(0, num_blocks):
             block = unique_text[i * block_size: i * block_size + block_size]
             thread = threading.Thread(target = translate_block, args = (block, word_count))
